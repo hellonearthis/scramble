7abc4cf38b1e94245e4ec4cc77379b3948009a7d
Brett Cooper <brettc@clear.net.nz>
2010-05-29 23:46:49 +1200

    changing to work at the bit level

--- tsting.php
+++ tsting.php
@@ -1,33 +1,55 @@
 <pre>
 <?php
-$thebytes='fgyewdskjafhajcyust9845n73q5c77-a8cj4w6c3fgyewdskjafhajcyust9845n73q5c77-a8cj4w6c3fgyewdskjafhajcyust9845n73q5c77-a8cj4w6c3fgyewdskjafhajcyust9845n73q5c77-a8cj4w6c3fgyewdskjafhajcyust9845n73q5c77-a8cj4w6c3';
-$tbytes=strlen($thebytes);
-$orgional_bytes=$thebytes;
+// this file to work out how to write the splitter
 
-echo 'file size '.$tbytes.'<br>';
-for ($scramble_sequence=0; $scramble_sequence < 16; $scramble_sequence++ ){ // 4 bit sequence as repetes
-// conver scramble_sequence into an arror of 0's and 1's
-  $skey = decbin($scramble_sequence);
-  $skey= str_repeat('0',(4-strlen($skey))); // make it 4 bits long
-  unset($sk);
-  $sk=str_split($skey).$skey;
-  print_r($sk);
-  echo ' $sk of sequence iteration '.$scramble_sequence.'<br>';
-$thebytes=$orgional_bytes;  // reset to orgional file bytes
-$x=0;
-  for ($i = 0; $i < 8; $i++) {  // filesize
-   // echo implode('',$sk);
- //   echo '<br>$i='.$i.'<br>';
-    if ($sk[$x] == 0){  //    put in $file0
-      echo $sk[$x].' $sk[$x] should be a 0<br>';
-    } else {  //   put in $file1
-      echo $sk[$x].' $sk[$x] should be a 1<br>';
-    }
+function byte2bin($input)  // convert  BYTE  into an 8 binary repesentation
+{
+  if (!is_string($input)) return null; // Sanity check
+  // Unpack as a hexadecimal string
 
-    $x++;
-    if ($x>3)$x=0;
-    echo '<br>'.$x.'= x '.$sk[$x].'= sk<br>';
-  }
+    $bb= decbin(ord($input));
+    $bb= str_repeat('0',(8-strlen($bb))).$bb; // make it 4 bits long
+     return $bb;
 }
+
+$thebytes='that fox is  quick as'.  chr(4).chr(254);  // bytes to scramble
+
+// convert to binary
+
+echo $thebytes.'<br>';
+
+//$numbytes=strlen($thebytes);  // number of bytes in the input file
+$tb=str_split($thebytes);
+
+foreach ($tb as $byte ) {   // loop through file byte by byte
+    echo $byte.':';
+    $bb= byte2bin($byte);   // make it binary
+    echo $bb[0].$bb[1].$bb[2].$bb[3].$bb[4].$bb[5].$bb[6].$bb[7].'<br>'; // easy access to each bit
+
+    // if key bit = 0 put bb bit in part 0
+    // if key bit = 1 put bb bit in part 1
+
+
+}
+
+
+//  $sk=str_split($skey).$skey;
+//  print_r($sk);                                 // the split key
+//$thebytes=$orgional_bytes;  // reset to orgional file bytes
+//$x=0;
+//  for ($i = 0; $i < 8; $i++) {  // filesize
+//   // echo implode('',$sk);
+// //   echo '<br>$i='.$i.'<br>';
+//    if ($sk[$x] == 0){  //    put in $file0
+//      echo $sk[$x].' $sk[$x] should be a 0<br>';
+//    } else {  //   put in $file1
+//      echo $sk[$x].' $sk[$x] should be a 1<br>';
+//    }
+//
+//    $x++;
+//    if ($x>3)$x=0;
+//    echo '<br>'.$x.'= x '.$sk[$x].'= sk<br>';
+//  }
+//}
 ?>
 </pre>
