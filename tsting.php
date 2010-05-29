79522881386e34dc44db8a867439101918aa6c67
Brett Cooper <brettc@clear.net.nz>
2010-05-30 02:47:41 +1200

    can take apart the test file

--- tsting.php
+++ tsting.php
@@ -3,53 +3,50 @@
 // this file to work out how to write the splitter
 
 function byte2bin($input)  // convert  BYTE  into an 8 binary repesentation
-{
-  if (!is_string($input)) return null; // Sanity check
-    $bb= decbin(ord($input));          // make the byte into binary
+{    $bb= decbin(ord($input));          // make the byte into binary
+    $bb= str_repeat('0',(8-strlen($bb))).$bb; // make it 8 bits long as decbinstrips high bits
+     return $bb;
+}
+
+function dec2bin($input)
+{   $bb= decbin($input);          // make the byte into binary
     $bb= str_repeat('0',(8-strlen($bb))).$bb; // make it 8 bits long as decbinstrips high bits
      return $bb;
 }
 
 $thebytes='that fox is  quick as'.  chr(4).chr(254);  // bytes to scramble, this will be the compress file.
 
-// convert to binary
-
 echo $thebytes.'<br>';
 
-//$numbytes=strlen($thebytes);  // number of bytes in the input file
-$tb=str_split($thebytes);
+$tb=str_split($thebytes); // convert input file into an array of bytes
 
 //  need to add for loop to cycle through split key (1-254) or just one key as this is the test area before working with compressed files.
-
+$scramkey=42;        // scramble key set
+$skey=dec2bin($scramkey);  // convert to binary
+echo $skey[0].$skey[1].$skey[2].$skey[3].$skey[4].$skey[5].$skey[6].$skey[7].' skey<br>';
+$P0='';
+$P1='';
 foreach ($tb as $byte ) {   // loop through file byte by byte
     echo $byte.':';
     $bb= byte2bin($byte);   // make it binary
     echo $bb[0].$bb[1].$bb[2].$bb[3].$bb[4].$bb[5].$bb[6].$bb[7].'<br>'; // faster access to each bit, skips splitting binary string into an array
 
-    // if key bit = 0 put bb bit in part 0
-    // if key bit = 1 put bb bit in part 1
-
-
+    // if key bit = 0 put bb bit in part 0 else put it into part 1
+   
+    for ($i=0; $i<=7; $i++){
+        if(($skey[$i]=='0')){
+            $P0.=$bb[$i];
+        } else {
+            $P1.=$bb[$i];
+        }
+    }
 }
 
+echo $P0.' p0<br>';
+echo $P1.' p1<br>';
+$p0len= strlen($P0);    // mid point used to unscramble the file.
+echo $p0len.' lenght of part o';
+// now to reverse it...
 
-//  $sk=str_split($skey).$skey;
-//  print_r($sk);                                 // the split key
-//$thebytes=$orgional_bytes;  // reset to orgional file bytes
-//$x=0;
-//  for ($i = 0; $i < 8; $i++) {  // filesize
-//   // echo implode('',$sk);
-// //   echo '<br>$i='.$i.'<br>';
-//    if ($sk[$x] == 0){  //    put in $file0
-//      echo $sk[$x].' $sk[$x] should be a 0<br>';
-//    } else {  //   put in $file1
-//      echo $sk[$x].' $sk[$x] should be a 1<br>';
-//    }
-//
-//    $x++;
-//    if ($x>3)$x=0;
-//    echo '<br>'.$x.'= x '.$sk[$x].'= sk<br>';
-//  }
-//}
 ?>
 </pre>
