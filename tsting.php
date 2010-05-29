e35f00fdacda1fd0bf1f05b8e2c5eaae730e66c0
Brett Cooper <brettc@clear.net.nz>
2010-05-29 23:49:20 +1200

    changing to work at the bit level

--- tsting.php
+++ tsting.php
@@ -12,7 +12,7 @@ function byte2bin($input)  // convert  BYTE  into an 8 binary repesentation
      return $bb;
 }
 
-$thebytes='that fox is  quick as'.  chr(4).chr(254);  // bytes to scramble
+$thebytes='that fox is  quick as'.  chr(4).chr(254);  // bytes to scramble, this will be the compress file.
 
 // convert to binary
 
@@ -21,10 +21,12 @@ echo $thebytes.'<br>';
 //$numbytes=strlen($thebytes);  // number of bytes in the input file
 $tb=str_split($thebytes);
 
+//  need to add for loop to cycle through split key (1-254) or just one key as this is the test area before working with compressed files.
+
 foreach ($tb as $byte ) {   // loop through file byte by byte
     echo $byte.':';
     $bb= byte2bin($byte);   // make it binary
-    echo $bb[0].$bb[1].$bb[2].$bb[3].$bb[4].$bb[5].$bb[6].$bb[7].'<br>'; // easy access to each bit
+    echo $bb[0].$bb[1].$bb[2].$bb[3].$bb[4].$bb[5].$bb[6].$bb[7].'<br>'; // faster access to each bit, skips splitting binary string into an array
 
     // if key bit = 0 put bb bit in part 0
     // if key bit = 1 put bb bit in part 1
